fce97eb33e856ee590415e28cf08ec91bbf4fccf
Christian Fuss <christian@hackyourshack.org>
2022-10-24 09:46:29 +0000

    Installed image and mdx integration and astro-m2dx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import m2dx from "astro-m2dx";
+
+/** @type {import('astro-m2dx').Options} */
+const m2dxOptions = {
+  // activate any required feature here
+  componentDirectives: true,
+  exportComponents: true,
+  relativeImages: true,
+  scanTitle: true,
+  scanAbstract: true,
+  styleDirectives: true,
+};
+
+export default defineConfig({
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx({
+      remarkPlugins: [[m2dx, m2dxOptions]],
+      extendDefaultPlugins: true,
+    }),
+  ],
+});
